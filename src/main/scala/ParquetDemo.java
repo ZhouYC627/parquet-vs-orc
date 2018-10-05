dbf2068b9a7eeda114303eb6cd3a9b3af0b0756b
Yichen Zhou <zhouycsf@gmail.com>
2018-10-05 21:05:32 +0800

    init

---
+++ src/main/scala/ParquetDemo.java
@@ -0,0 +1,44 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+
+public class ParquetDemo {
+
+    public static void main(String[] args) {
+
+        SparkConf conf = new SparkConf()
+                .setAppName("ParquetTest")
+                .set("spark.sql.parquet.filterPushdown", "true");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        SQLContext sqlContext = new SQLContext(sc.sc());
+
+        sqlContext.sql("use ssb");
+        sqlContext.sql("show tables").show();
+        //sqlContext.sql("SELECT COUNT(*) FROM p_lineorder").show();
+        /*
+        sqlContext.sql("select sum(v_revenue) as revenue" +
+                " from p_lineorder" +
+                " group by d_year");
+         */
+        //sqlContext.sql("select d_year,sum(v_revenue) as revenue from p_lineorder left join dates on lo_orderdate = d_datekey group by d_year");
+        Dataset<Row> df =
+                sqlContext.sql("select d_date, sum(v_revenue) as revenue from p_lineorder left join dates on lo_orderdate = d_datekey group by d_datekey");
+
+        //df.show();
+        String parquetFileName = "sum_revenue.parquet";
+        df.write().parquet(parquetFileName);
+        System.out.print("Saving to " + parquetFileName);
+
+
+        //Dataset<Row> parquetFileDF = spark.read().parquet("people.parquet");
+
+
+        sc.close();
+    }
+
+
+
+}
